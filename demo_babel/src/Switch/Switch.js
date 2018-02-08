d186541cee1a794f5ff66bf6387128b97706c98a
Ajesh Babu <ajesh@QB-TP3-1129.local>
2018-02-08 10:33:15 +0530

    Context children demo project added

---
+++ demo_babel/src/Switch/Switch.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'; 
+import './../App.css';
+
+function Switch({on, className="", ...props}){
+
+	return (
+		<div className="toggle">
+	      <input
+	        className="toggle-input"
+	        type="checkbox"
+	      />
+	      <button className={`${className} toggle-btn ${on ? 'toggle-btn-on' : 'toggle-btn-off'}`} {...props}/>
+	    </div>
+		)
+
+}
+
+export default Switch;
---
+++ demo_babel/src/Switch/Toggle.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react'; 
+import PropTypes from 'prop-types';
+
+
+import Switch from './Switch';
+
+
+const TOGGLE_CONTEXT = '__toggle__'
+
+function withToggle(Component) {
+
+  function Wrapper(props, context) {
+    const toggleContext = context[TOGGLE_CONTEXT]
+    return (
+      <Component {...toggleContext} {...props} />
+    )
+  }
+
+  Wrapper.contextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  }
+  
+  return Wrapper
+}
+
+
+const ToggleOn = withToggle(({children, on}) => {
+  return on ? children : null
+})
+
+const ToggleOff = withToggle(( {children, on }) => {
+  return on ? null : children
+})
+
+// function ToggleButton({on, toggle, ...props}) {
+//   return (
+//     <Switch on={on} onClick={toggle} {...props} />
+//   )
+// }
+
+const ToggleButton = withToggle( ({on, toggle, ...props} ) => {
+    return (
+      <Switch on={on} onClick={toggle} {...props} />
+    )
+  },
+)
+
+
+class Toggle extends React.Component {
+  static On = ToggleOn
+  static Off = ToggleOff
+  static Button = ToggleButton
+  static defaultProps = {onToggle: () => {}}
+  static childContextTypes = {
+    [TOGGLE_CONTEXT]: PropTypes.object.isRequired,
+  }
+
+  state = {on: false}
+  toggle = () =>
+    this.setState(
+      ({on}) => ({on: !on}),
+      () => this.props.onToggle(this.state.on),
+    )
+  getChildContext() {
+    return {
+      [TOGGLE_CONTEXT]: {
+        on: this.state.on,
+        toggle: this.toggle,
+      },
+    }
+  }
+  render() {
+    return <div>{this.props.children}</div>
+  }
+}
+
+
+export default Toggle;
