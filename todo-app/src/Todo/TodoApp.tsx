0114ceb733abe14e00c5e1b54e825ecb9936ae56
Ajesh Babu <ajesh@QB-TP3-1129.local>
2018-02-08 21:43:17 +0530

    Input added in todo

--- todo-app/src/Todo/TodoApp.tsx
+++ todo-app/src/Todo/TodoApp.tsx
@@ -4,11 +4,28 @@ import TodoList from './TodoList';
 class TodoApp extends React.Component<{}, { items: Array<string> }> {
   constructor(props: any) {
     super(props);
-    const itemsArray = ["apple", "orange", "jackfruit"];
+    const itemsArray = ['Apple'];
     this.state = { items: itemsArray };
   }
+  addItem = (event: any) => {
+    if (event.keyCode == 13) {
+      this.state.items.push(event.target.value);
+    }
+  };
   render() {
-    return <TodoList items={this.state.items} />;
+    return (
+      <div className="todo-box">
+        <div className="row todo-box-input">
+          <input
+            type="text"
+            className="todo-input"
+            name="todo-input"
+            onKeyUp={this.addItem.bind(this)}
+          />
+        </div>
+        <TodoList items={this.state.items} />
+      </div>
+    );
   }
 }
 
