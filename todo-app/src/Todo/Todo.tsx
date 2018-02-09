0ed33852e7cc39c9f5623d5f94318cba3420007d
Ajesh Babu <ajesh@QB-TP3-1129.local>
2018-02-09 08:59:42 +0530

    Todo made stateless

--- todo-app/src/Todo/Todo.tsx
+++ todo-app/src/Todo/Todo.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import './../App.css';
-class Todo extends React.Component<{ value: string }> {
-  render() {
-    return (
+const Todo: React.SFC<{ value: string }> = ({value}) =>
+  (
     <div className="row todo-details">
       <input className="col-sm-2 " type="checkbox" name="tick" />
-      <p className="col-sm-8 todo-name">{this.props.value}</p>
+      <p className="col-sm-8 todo-name">{value}</p>
       <button className="col-sm-2 delete-button">X</button>
-    </div>)
-  }
-}
+    </div>
+  )
+ 
 
 export default Todo;
