1992cf63980320b30d1ee8371751d6eef5d9f7b0
Ajesh Babu <ajesh@QB-TP3-1129.local>
2018-02-12 10:12:39 +0530

    Edit changes

--- todo-app/src/Todo/Todo.tsx
+++ todo-app/src/Todo/Todo.tsx
@@ -5,33 +5,57 @@ type TProps = {
   value: string;
   index: number;
   onDelete: Function;
-}
+};
 
-export default class Todo extends React.Component<TProps> {
+class Todo extends React.Component<TProps,{showEdit: boolean, value: string}> {
   constructor(props: TProps) {
-    super(props)
-
-    this.localDelete = this.localDelete.bind(this)
+    super(props);
+    this.state = {showEdit: false,value: props.value}
+    this.localDelete = this.localDelete.bind(this);
+    // this.enableEditing = this.enableEditing.bind(this);
+    this.makeValueChange = this.makeValueChange.bind(this);
+    this.editValue = this.editValue.bind(this);
   }
 
   localDelete() {
     // show confirm??
     const { index } = this.props;
+    
+    this.props.onDelete(index);
+  }
 
-    this.props.onDelete(index)
+  enableEditing = () => {
+     this.setState({showEdit: true});
+  }
+
+  makeValueChange = (event: any) => {
+    this.setState({value: event.target.value});
+  }
+
+  editValue = (event: any) => {
+    if (event.keyCode == 13) {
+      event.stopPropagation();
+      this.editTodoTitle();
+      this.setState({showEdit: false});
+    }
+  };
+
+  editTodoTitle = () => {
+     console.log('change Value');
   }
 
   render() {
-    const { value } = this.props;
 
     return (
-      <div className="row todo-details">
-      <input className="col-sm-2 " type="checkbox" name="tick" />
-      <p className="col-sm-8 todo-name">{value}</p>
-      <button className="col-sm-2 delete-button" onClick={this.localDelete}>X</button>
-    </div>
-    )
+      <div className="row todo-details" onDoubleClick={this.enableEditing} >
+        <input className="col-sm-2 " type="checkbox" name="tick" />
+        <input className="col-sm-8 todo-name" type="text" value={this.state.value} onBlur={this.editTodoTitle} onKeyUp={this.editValue} onChange={this.makeValueChange} readOnly={!this.state.showEdit} />
+        <button className="col-sm-2 delete-button" onClick={this.localDelete}>
+          X
+        </button>
+      </div>
+    );
   }
 }
 
-// export default Todo;
+export default Todo;
