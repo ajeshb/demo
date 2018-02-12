5f2e90c1ad7c39efcdde1907e4e14d9b93af1dd9
Ajesh Babu <ajesh@QB-TP3-1129.local>
2018-02-12 10:14:40 +0530

    Minor changes

--- todo-app/src/Todo/Todo.tsx
+++ todo-app/src/Todo/Todo.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import './../App.css';
+import * as React from "react";
+import "./../App.css";
 
 type TProps = {
   value: string;
@@ -7,10 +7,13 @@ type TProps = {
   onDelete: Function;
 };
 
-class Todo extends React.Component<TProps,{showEdit: boolean, value: string}> {
+class Todo extends React.Component<
+  TProps,
+  { showEdit: boolean; value: string }
+> {
   constructor(props: TProps) {
     super(props);
-    this.state = {showEdit: false,value: props.value}
+    this.state = { showEdit: false, value: props.value };
     this.localDelete = this.localDelete.bind(this);
     // this.enableEditing = this.enableEditing.bind(this);
     this.makeValueChange = this.makeValueChange.bind(this);
@@ -20,36 +23,43 @@ class Todo extends React.Component<TProps,{showEdit: boolean, value: string}> {
   localDelete() {
     // show confirm??
     const { index } = this.props;
-    
     this.props.onDelete(index);
   }
 
   enableEditing = () => {
-     this.setState({showEdit: true});
-  }
+    this.setState({ showEdit: true });
+  };
 
   makeValueChange = (event: any) => {
-    this.setState({value: event.target.value});
-  }
+    this.setState({ value: event.target.value });
+  };
 
   editValue = (event: any) => {
     if (event.keyCode == 13) {
       event.stopPropagation();
       this.editTodoTitle();
-      this.setState({showEdit: false});
+      this.setState({ showEdit: false });
     }
   };
 
   editTodoTitle = () => {
-     console.log('change Value');
-  }
+    if(this.state.showEdit)
+      console.log("change Value");
+  };
 
   render() {
-
     return (
-      <div className="row todo-details" onDoubleClick={this.enableEditing} >
+      <div className="row todo-details" onDoubleClick={this.enableEditing}>
         <input className="col-sm-2 " type="checkbox" name="tick" />
-        <input className="col-sm-8 todo-name" type="text" value={this.state.value} onBlur={this.editTodoTitle} onKeyUp={this.editValue} onChange={this.makeValueChange} readOnly={!this.state.showEdit} />
+        <input
+          className="col-sm-8 todo-name"
+          type="text"
+          value={this.state.value}
+          onBlur={this.editTodoTitle}
+          onKeyUp={this.editValue}
+          onChange={this.makeValueChange}
+          readOnly={!this.state.showEdit}
+        />
         <button className="col-sm-2 delete-button" onClick={this.localDelete}>
           X
         </button>
